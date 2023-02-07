17db6f98f30d2ce1fd9b479d64c172a86024b58e
Gaurav-Vish <gvishwakarma768@gmail.com>
2023-02-07 00:44:25 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import ReactDOM from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const SkiResort = () => {
+  const [resorts, setResorts] = useState([]);
+  const [newResort, setNewResort] = useState({ name: '', location: '', runs: '' });
+  const [editId, setEditId] = useState(null);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const handleChange = (e) => {
+    setNewResort({ ...newResort, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newResort.name || !newResort.location || !newResort.runs) return;
+    if (editId !== null) {
+      setResorts(resorts.map((resort) => (resort.id === editId ? { ...newResort, id: editId } : resort)));
+      setEditId(null);
+    } else {
+      setResorts([...resorts, { ...newResort, id: resorts.length + 1 }]);
+    }
+    setNewResort({ name: '', location: '', runs: '' });
+  };
+
+  const handleEdit = (id) => {
+    setEditId(id);
+    setNewResort(resorts.find((resort) => resort.id === id));
+  };
+
+  const handleDelete = (id) => {
+    setResorts(resorts.filter((resort) => resort.id !== id));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={newResort.name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Location"
+          name="location"
+          value={newResort.location}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Runs"
+          name="runs"
+          value={newResort.runs}
+          onChange={handleChange}
+        />
+        <button type="submit">Save</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Location</th>
+            <th>Runs</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {resorts.map((resort) => (
+            <tr key={resort.id}>
+              <td>{resort.name}</td>
+              <td>{resort.location}</td>
+              <td>{resort.runs}</td>
+              <td>
+                <button onClick={() => handleEdit(resort.id)}>Edit</button>
+                <button onClick={() => handleDelete(resort.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<SkiResort/>);
